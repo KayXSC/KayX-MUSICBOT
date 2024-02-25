33c7c526eae22905397ebb83891e87ea205733d9
KayX <66139977+KayXSC@users.noreply.github.com>
2024-02-25 17:28:13 +0100

    Update stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -1,14 +1,23 @@
-const { getVoiceConnection } = require('@discordjs/voice');
+const { getVoiceConnection, AudioPlayerStatus } = require('@discordjs/voice');
 
 module.exports = {
     name: 'stop',
-    execute: async function(message) {
+    execute: async function(message, queues) {
         const connection = getVoiceConnection(message.guild.id);
         if (!connection) {
-            message.reply('I am not playing any music in this guild.');
+            message.reply('No estoy reproduciendo ninguna música en este servidor.');
             return;
         }
-        connection.destroy();
-        message.reply('Stopped playing music.');
+
+        const player = connection.state.subscription.player;
+        if (player.state.status !== AudioPlayerStatus.Idle) {
+            player.stop();
+        }
+
+        if (queues.has(message.guild.id)) {
+            queues.get(message.guild.id).length = 0;
+        }
+
+        message.reply('He dejado de reproducir música y he limpiado la cola de canciones.');
     }
 };
